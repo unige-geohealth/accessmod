969fed8ce68a9f9403fc4d2bbe3062f2c6369e7e
fxi <f@fxi.io>
2015-05-04 16:58:34 +0200

    comments

--- modules/amManageSettings/amServer.R
+++ modules/amManageSettings/amServer.R
@@ -4,8 +4,7 @@
 #   / ___ |/ /__ / /__ /  __/(__  )(__  )/ /  / // /_/ // /_/ /  ____/ /
 #  /_/  |_|\___/ \___/ \___//____//____//_/  /_/ \____/ \__,_/  /_____/
 #
-# description of the project 
-# TODO : link the wiki here.
+# settings and admin task 
 
 
 
