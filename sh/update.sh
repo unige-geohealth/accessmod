1dbf3373a73d519687707cfd18f680f2ffd72d20
fxi <f@fxi.io>
2016-01-06 16:23:35 +0100

    should work

--- sh/update.sh
+++ sh/update.sh
@@ -5,43 +5,50 @@ os="Linux"
 gitHost="github.io"
 logPath="/srv/shiny-server/logs/logs.txt"
 
-if [[ `hostname` == $hostname && `uname` == $os ]]
+
+
+if [ "`hostname`" == "$hostname" -a "`uname`" == "$os" ]
 then
-  gitPing=`ping -c1 $gitHost` 
-  gitOk= `echo $gitPing | grep "\s0% packet loss"  | wc -l`
-
-  msgNoGit=`date +"%Y-%m-%d"`" \t warning \t $gitHost not reachable. $gitPing " 
-  msgNoUpdate=`date +"%Y-%m-%d"`" \t log \t No update. " 
-  msgUpdateDone=`date +"%Y-%m-%d"`" \t log \t Update done " 
-
-  if [ $gitOk -eq 1 ]
+  if [ ! -e "$logPath" ]
   then
-    # fetch changes, git stores them in FETCH_HEAD
-    git fetch
-
-    # check for remote changes in origin repository
-    newUpdatesAvailable=`git diff HEAD FETCH_HEAD`
-    if [ "$newUpdatesAvailable" != "" ]
-    then
-      # create the fallback
-      git branch fallbacks
-      git checkout fallbacks
-
-      git add .
-      git add -u
-      git commit -m `date "+%Y-%m-%d"`
-      echo "fallback created"
-
-      git checkout master
-      git merge FETCH_HEAD
-      echo "merged updates"
-      touch restart.txt
-      echo -e $msgUpdateDone >> logPath
-    else
-      echo "no updates available"
-      echo -e $msgUpdateDone >> logPath
-    fi
+    echo "$logPath not found"
   else
-    echo -e $msgNoGit >> logPath
+    gitPing=`ping -c1 $gitHost`
+    gitOk=`echo $gitPing | grep "\s0% packet loss"  | wc -l`
+
+    msgNoGit=`date +"%Y-%m-%d"`" \t warning \t $gitHost not reachable. $gitPing "
+    msgNoUpdate=`date +"%Y-%m-%d"`" \t log \t No update. "
+    msgUpdateDone=`date +"%Y-%m-%d"`" \t log \t Update done "
+
+    if [ "$gitOk" -eq 1 ]
+    then
+      # fetch changes, git stores them in FETCH_HEAD
+      git fetch
+
+      # check for remote changes in origin repository
+      newUpdatesAvailable=`git diff HEAD FETCH_HEAD`
+      if [ "$newUpdatesAvailable" != "" ]
+      then
+        # create the fallback
+        git branch fallbacks
+        git checkout fallbacks
+
+        git add .
+        git add -u
+        git commit -m `date "+%Y-%m-%d"`
+        echo "fallback created"
+
+        git checkout master
+        git merge FETCH_HEAD
+        echo "merged updates"
+        touch restart.txt
+        echo -e "$msgUpdateDone" >> $logPath
+      else
+        echo "no updates available"
+        echo  "$msgNoUpdate" >> "$logPath"
+      fi
+    else
+      echo -e "$msgNoGit" >> "$logPath"
+    fi
   fi
 fi
