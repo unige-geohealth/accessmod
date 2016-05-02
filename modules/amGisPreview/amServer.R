0bf3276ac59e0116cfd9d5d9f15f911b7454e20e
fxi <f@fxi.io>
2016-05-02 22:30:02 +0200

    use new leaflet package; selection of a basemap or empty basemap;

--- modules/amGisPreview/amServer.R
+++ modules/amGisPreview/amServer.R
@@ -5,54 +5,63 @@
 #  /_/  |_|\___/ \___/ \___//____//____//_/  /_/ \____/ \__,_/  /_____/
 ## map preview 
 
+source("tools/R/amLeafletPatch.R")
+
 
 observe({
+
   amModEnabled<-listen$tabControl_module_preview  
+  
   if(isTRUE(!is.null(amModEnabled) && amModEnabled)){
     # directory for map cache
     addResourcePath('mapCache',config$pathCacheDir)
     # create leaflet map
     #amMap <- createLeafletMap(session, "amMap")
-    amPreviewMap <- createLeafletMap(session, "amPreviewMap")
+#    amPreviewMap <- createLeafletMap(session, "amPreviewMap")
+
+    output$mapPreview <- renderLeaflet({
+      leaflet()%>%
+      addScaleBar()%>%
+      mapOptions(zoomToLimits = "first")
+    })
 
 
+
+   satArcGisAttrib<-"i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
+   satLayer <-  "http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+
+    observe({
+      if(input$selBaseMap != "empty"){
+        leafletProxy("mapPreview") %>%
+          addProviderTiles(
+            input$selBaseMap,
+            layerId="baselayer"
+            )
+      }else{
+        leafletProxy("mapPreview") %>%
+          removeTiles("baselayer") 
+      }
+    })
 
 
     observe({
-      maps<-dataList$raster
+      maps <- dataList$raster
       updateSelectInput(session,"mapToPreview",choices=maps)
     })
 
-    observe({ 
-      ready=length(input$amPreviewMap_bounds)==4
-      listen$previewMapReady<-ifelse(ready,TRUE,FALSE)
-    })
-
-    # if the location change and if the map is ready, change extent geojson.
-    changePreviewExtent<-reactive({
-      mapReady<-listen$previewMapReady
+    observe({
       m <- listen$mapMeta
-      if(!is.null(m) && isTRUE(mapReady)){
-        amPreviewMap$addGeoJSON(amBboxGeoJson(m,proj='latlong'),'extent')
-        bbx<-as.numeric(unlist(m$latlong$bbx$ext))
-        amPreviewMap$fitBounds(bbx[3],bbx[2],bbx[4],bbx[1]) 
-      }
+      bbx<-as.numeric(unlist(m$latlong$bbx$ext))
+      leafletProxy("mapPreview") %>%
+        fitBounds(bbx[1],bbx[3],bbx[2],bbx[4]) 
     })
 
 
 
-    # if mapToPreview change, evaluate reactive expression.
     observe({
-      mapToPreview<-input$mapToPreview
-      if(!is.null(mapToPreview) && !mapToPreview==""){
-        changePreviewExtent()
-      }
-    })
-
-    observe({
-      clickCoord<-input$amPreviewMap_click
+      clickCoord <- input$mapPreview_click
+      mapToPreview <- amNameCheck(dataList,input$mapToPreview,'raster')
       isolate({
-        mapToPreview<-amNameCheck(dataList,input$mapToPreview,'raster')
         if(!is.null(mapToPreview) && !is.null(clickCoord)){
           clickCoord<-c(x=clickCoord$lng, y=clickCoord$lat)
           tbl<-amRastQueryByLatLong(
@@ -60,7 +69,6 @@ observe({
             mapToPreview,
             projOrig=listen$mapMeta$orig$proj,
             projDest=listen$mapMeta$latlong$proj)
-          #  res<-data.frame(longitude=clickCoord['x'],latitude=clickCoord['y'],value=res)
         }else{
           tbl=data.frame(long='-',lat='-',value='-',label='-')
         }
@@ -69,21 +77,22 @@ observe({
     })
 
     # set a region to extract from grass
-    observe({
+    observe({ 
+
+  amErrorAction({
       # preview parameter list
-      pL              = list(
-        # reactive
-        leafletBounds = input$amPreviewMap_bounds,# leaflet bounds change
+      pL = list(
+        leafletBounds = input$mapPreview_bounds,# leaflet bounds change
         mapToPreview  = amNameCheck(dataList,input$mapToPreview,'raster'), # map from dataList$raster
         opacity       = input$previewOpacity, # opacity change
-        # isolate
-        mapReady      = isolate(listen$previewMapReady),
         meta      = isolate(listen$mapMeta)
         )
+
       isolate({
-        ready<-!any(FALSE %in% pL || TRUE %in% sapply(pL,is.null))
+        ready <- !any(TRUE %in% sapply(pL,is.null))
         if(ready){
-          amErrorAction({
+
+
             # render map : png path and boundingbox
             mapPreview<-amGrassLatLongPreview(
               mapToPreview=pL$mapToPreview,
@@ -91,29 +100,59 @@ observe({
               bbxSpLatLongOrig=amBboxSp(pL$meta,proj='latlong'),
               mapCacheDir=config$pathCacheDir,
               resGrassEW=pL$meta$grid$ewres,
-              resMax=400,
+              showLegend=pL$showLegend,
+              resMax=300,
               projOrig=listen$mapMeta$orig$proj,
               projDest=listen$mapMeta$latlong$proj 
               )
-            if(is.null(mapPreview))return(NULL)
-            # retrieve resulting intersecting bounding box
-            bbx<-mapPreview$bbx
-            # from local path to mapCache path,  registered as external ressource for shiny. (addRessourcePath)
-            previewPath<-file.path('mapCache',basename(mapPreview$pngFile))
 
-            # send data to map 
-            amPreviewMap$addOverlay(
-              bbx['y','min'],bbx['x','min'],
-              bbx['y','max'],bbx['x','max'],
-              previewPath, 'preview-test',
+            if(is.null(mapPreview)) return(NULL)
+
+            # where to save image cache
+            previewPath<-file.path('mapCache',basename(mapPreview$pngMap))
+
+           
+            # legend html
+
+            if(file.exists(mapPreview$pngLegend)){ 
+              legendPath <- file.path('mapCache',basename(mapPreview$pngLegend))
+              leg <- tagList(
+                conditionalPanel(condition="input.checkShowLegend",{
+                  img(src=legendPath)
+})
+                )
+            }else{
+             leg <- div("")
+            }
+            
+
+            # retrieve resulting intersecting bounding box
+            bbx <- mapPreview$bbx
+
+            leafletProxy("mapPreview") %>%
+              addControl(
+                html=leg,
+                position="topright",
+                layerId="layerPreviewLegend"
+                ) %>%
+            addPng(
+              layerId="layerPreview",
+              lat1=bbx['y','min'],
+              lng1=bbx['x','min'],
+              lat2=bbx['y','max'],
+              lng2=bbx['x','max'],
+              imgUrl=previewPath,
               options=list(
                 opacity=pL$opacity
                 )
               )
-          },title='Map preview generator')
+
         }
+        })
+
+      },title='Map preview generator')
       })
-    })
-  }
+        }
+
 })
 
--- modules/amGisPreview/amUi.R
+++ modules/amGisPreview/amUi.R
@@ -1,18 +1,16 @@
 
 
 
+tileProviders = list(
+  "Simple I" = "CartoDB.PositronNoLabels",
+  "Dark" = "CartoDB.DarkMatterNoLabels",
+  "Outdoors" = "Thunderforest.Outdoors",
+  "Terrain" ="Esri.WorldTerrain",
+  "Satellite I" = "Esri.WorldImagery",
+  "Empty" = "empty"
+  )
 
 
-# other map tiles providers :
-#initialTileLayer = "//{s}.tiles.mapbox.com/v3/fxi.801dac55/{z}/{x}/{y}.png",
-#initialTileLayer = "//{s}.tiles.mapbox.com/v3/fxi.l2o6dd72/{z}/{x}/{y}.png",
-#initialTileLayer="http://a{s}.acetate.geoiq.com/tiles/terrain/{z}/{x}/{y}.png",
-#initialTileLayer="http://otile1.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.jpg",
-#initialTileLayer="http://{s}.tile.osm.org/{z}/{x}/{y}.png",
-#initialTileLayer="http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png",
-#initialTileLayerAttribution = HTML('tiles:acetate.geoiq.com,data:OSM'),
-#arcgisonline attribution
-arcGisAttrib<-"i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
 
 
 fluidRow(
@@ -36,22 +34,19 @@ fluidRow(
         ),
       column(width=4,
         h4('Map display'),
-        sliderInput('previewOpacity','Set data opacity',min=0,max=1,value=0.8,step=0.1)
+        sliderInput('previewOpacity','Set data opacity',min=0,max=1,value=0.8,step=0.1),
+        selectizeInput("selBaseMap","Select a basemap",tileProviders),
+        checkboxInput("checkShowLegend","Show legend",value=FALSE)
         )
       )
     ),
   mainPanel(width=12,
-    leafletMap(
-      "amPreviewMap", "100%", 500,
-      initialTileLayer = 
-      'http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-      initialTileLayerAttribution = HTML(arcGisAttrib),
-      options=list(
-        center = c(0,0),
-        zoom=2,
-        maxZoom = 18,
-        zoomControl=FALSE
-        )
-      )
+   tagList(
+    leafletOutput("mapPreview","100%","500")
     )
+   )
   )
+
+#arcGisAttrib<-"i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
+    #  #'http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+    #  #,HTML(arcGisAttrib)
