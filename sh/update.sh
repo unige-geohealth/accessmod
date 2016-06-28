b184014031614caf9ca5154f842c260b57a1a6c0
fxi <f@fxi.io>
2016-06-28 17:16:47 +0200

    added git-fetch tags

--- sh/update.sh
+++ sh/update.sh
@@ -36,7 +36,9 @@ then
        # get current branch
       currentBranch=$(git branch | grep '*' |awk '{ print $2}')
       # fetch changes, git stores them in FETCH_HEAD
-      git fetch origin $currentBranch
+
+      git fetch --tags origin $currentBranch
+      git fetch  origin $currentBranch
 
       # check for remote changes in origin repository
       newUpdatesAvailable=`git diff HEAD FETCH_HEAD`
