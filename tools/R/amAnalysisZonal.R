54196f969dabfba330a1da172131141d7003dc01
fxi <f@fxi.io>
2018-10-06 11:36:03 +0200

    Solves reclass issue described in #202

--- tools/R/amAnalysisZonal.R
+++ tools/R/amAnalysisZonal.R
@@ -34,52 +34,17 @@ amZonalAnalysis <- function(
     if(tmpZoneExists){
 
 
-      # reclass travel time to keep only the slider value
-      tmpRulesFile<-tempfile()
-      tmpRules <- sprintf(
-        "0 thru %1$s = %1$s travelTime",
-        timeCumCost
-        )
-      write(tmpRules,tmpRulesFile)
-      execGRASS('r.reclass',
-        input=inputTravelTime,
-        output='tmp__cum_cost',
-        rules=tmpRulesFile,
-        flags='overwrite'
-        )
       # extract population under coverage area.
 
       popUnderTravelTime <- sprintf(
-        "tmp__pop_under_travel_time = if(tmp__cum_cost==%1$s,%2$s,null())",
+        "tmp__pop_under_travel_time = %1$s <= %2$s ? %3$s : null()",
+        inputTravelTime,
         timeCumCost,
         inputPop
         )
 
       execGRASS('r.mapcalc',expression=popUnderTravelTime,flags='overwrite')
 
-      # produce map
-      #tmpMapTt<-file.path(tempdir(),'mapTravelTime.tiff')
-    
-      #execGRASS('g.region',res=paste(resolution*nColumns/500))
-      #exp=paste('tmp__cum_cost_preview=if(',inputTravelTime,'<',timeCumCost,',',inputTravelTime,',null())')
-     # execGRASS('r.mapcalc',expression=exp,flags='overwrite')
-      
-      #execGRASS('r.out.gdal',
-        #flags =c('overwrite'),
-        #input='tmp__cum_cost_preview',
-        #output=tmpMapTt,
-        #format="GTiff",
-        #createopt='TFW=YES'
-        #)
-      #execGRASS('g.region',raster=mapDem)
-      #rTt<-raster(tmpMapTt)
-
-      ### labels
-      #labeltext = paste("Travel time",timeCumCost,"[min]")
-
-      ### plot
-      #res$plot <- rTt 
-       
       statZonePopTravelTime<-read.table(text=
         execGRASS('r.univar',
           map='tmp__pop_under_travel_time',
