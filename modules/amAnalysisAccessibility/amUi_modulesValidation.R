e41baeee63df260e431d75bf69a0fde38f093c1d
Carlos Ochoa <carlosrochoa@protonmail.com>
2019-01-21 16:31:49 +0100

    changes of = signs, vertical structure and amt function

--- modules/amAnalysisAccessibility/amUi_modulesValidation.R
+++ modules/amAnalysisAccessibility/amUi_modulesValidation.R
@@ -1,18 +1,29 @@
 wellPanel(
-  amCenterTitle(div(icon('check-square-o'),'Validation'),m=0,h=3,sub=
-    "Review validation issues and compute."
+  amCenterTitle(div(icon('check-square-o'), amt(
+   id = "analysis_validation",
+   str = 'Validation'
+   )),
+   m = 0,
+   h = 3,
+   sub = "Review validation issues and compute."
   ),
 uiOutput('msgModule3'),
-conditionalPanel(condition="(
+conditionalPanel(condition = "(
   input.moduleSelector=='module_5'
   )",
-actionButton("btnZonalStat","Update")
+  actionButton("btnZonalStat", amt(
+    id = "analysis_validation_update",
+	str = "Update"
+	))
 ),
-conditionalPanel(condition="(
+conditionalPanel(condition = "(
   input.moduleSelector!='module_5'
   )",
-actionButton('btnComputeAccessibility','Compute')
-)
+  actionButton('btnComputeAccessibility', amt(
+    id = "analysis_validation_compute",
+	str = 'Compute'
+	))
+  )
 )
 
 
