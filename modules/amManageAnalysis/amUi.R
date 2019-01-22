6abac37e29632a14927acb0b5329c9c2c13311bc
Carlos Ochoa <carlosrochoa@protonmail.com>
2019-01-22 17:23:47 +0100

    changes of = signs, vertical structure and amt function

--- modules/amManageAnalysis/amUi.R
+++ modules/amManageAnalysis/amUi.R
@@ -6,49 +6,83 @@
 #
 # Module selector / top menu in analysis
 
-moduleInlineBtn<-div(id='moduleSelector',
-  class="form-group shiny-input-radiogroup shiny-input-container-inline shiny-flow-layout",
-  'data-toggle'='buttons',style='line-height:37px;',
-  tags$label('for'='moduleSelector'),
-  tags$label(class="btn btn-default active btn-inline w6-100",
-    tags$input(type="radio",id="ms2",name="moduleSelector",value='module_2',checked='checked'),
-    icon('clock-o'),'Accessibility'
+moduleInlineBtn <- div(id = 'moduleSelector',
+  class = "form-group shiny-input-radiogroup shiny-input-container-inline shiny-flow-layout",
+    'data-toggle' = 'buttons',
+	style = 'line-height:37px;',
+	tags$label('for' = 'moduleSelector'),
+	tags$label(class = "btn btn-default active btn-inline w6-100",
+    tags$input(type = "radio",
+	  id = "ms2",
+	  name = "moduleSelector",
+	  value = 'module_2',
+	  checked = 'checked'
+	  ),
+    icon('clock-o'), amt(
+	  id = "analysis_access_tab",
+	  str = 'Accessibility'
+	  )
     ),
-  tags$label(class="btn btn-default btn-inline w6-100",
-    tags$input(type="radio",id="ms3",name="moduleSelector",value='module_3'),
-    icon('hospital-o'),'Geographic coverage'
+  tags$label(class = "btn btn-default btn-inline w6-100",
+    tags$input(type = "radio",
+	  id = "ms3",
+	  name = "moduleSelector",
+	  value = 'module_3'),
+    icon('hospital-o'), amt(
+	  id = "analysis_coverage_tab",
+	  str = 'Geographic coverage'
+	  )
     ),
-  tags$label(class="btn btn-default btn-inline w6-100 ",
-    tags$input(type="radio",id="ms4",name="moduleSelector",value='module_4'),
-    icon('table'),'Referral'
+  tags$label(class = "btn btn-default btn-inline w6-100 ",
+    tags$input(type = "radio",
+	  id = "ms4",
+	  name = "moduleSelector",
+	  value = 'module_4'),
+    icon('table'), amt(
+	  id = "analysis_referral_tab",
+	  str = 'Referral'
+	  )
     ),
-  tags$label(class="btn btn-default btn-inline w6-100",
-    tags$input(type="radio",id="ms5",name="moduleSelector",value='module_5'),
-    icon('bar-chart'),'Zonal statistics'
+  tags$label(class = "btn btn-default btn-inline w6-100",
+    tags$input(type = "radio",
+	  id = "ms5", 
+	  name = "moduleSelector",
+	  value = 'module_5'),
+    icon('bar-chart'), amt(
+	  id = "analysis_stats_tab",
+	  str = 'Zonal statistics'
+	  )
+    ),
+  tags$label(class = "btn btn-default btn-inline w6-100 ",
+    tags$input(type = "radio",
+	  id = "ms6",
+	  name = "moduleSelector",
+	  value = 'module_6'),
+    icon('plus'), amt(
+	  id = "analysis_scaleup_tab",
+	  str = 'Scaling up'
+	  )
     )
-   ,tags$label(class="btn btn-default btn-inline w6-100 ",
-     tags$input(type="radio",id="ms6",name="moduleSelector",value='module_6'),
-     icon('plus'),'Scaling up'
-     )
   )
 
 
-
 fluidRow(
-  column( width=12,HTML(gsub('\n','',moduleInlineBtn))),
+  column( width = 12,
+    HTML(gsub('\n', '', moduleInlineBtn
+	))),
   hr(),
-  column(width=12,
- #   conditionalPanel(condition="
+  column(width = 12,
+ #   conditionalPanel(condition = "
       #input.moduleSelector=='module_1'
       #",loadUi('modules/amAnalysisMergeLandCover/amUi.R')
       #),
-    conditionalPanel(condition="
+    conditionalPanel(condition = "
       input.moduleSelector=='module_2' || 
       input.moduleSelector=='module_3' || 
       input.moduleSelector=='module_4' ||
       input.moduleSelector=='module_5' ||
        input.moduleSelector=='module_6'  
-      ",loadUi('modules/amAnalysisAccessibility/amUi.R')
+      ", loadUi('modules/amAnalysisAccessibility/amUi.R')
       )
     )
   )
