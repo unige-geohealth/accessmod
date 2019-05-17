1dae1d56d5bb4d66de61874b237606fec507c489
fxi <f@fxi.io>
2019-05-17 12:05:32 +0200

    Change update strategy; add script to automate update version process

--- sh/update.sh
+++ sh/update.sh
@@ -4,6 +4,7 @@ dateStamp=`date "+%Y-%m-%d@%H_%M_%S"`
 hostname="accessmod"
 dirCurrent=basename`pwd`
 dirTest="accessmod_latest"
+origin="origin"
 os="Linux"
 gitHost="github.com"
 appPath="/srv/shiny-server/accessmod"
@@ -36,14 +37,13 @@ then
   then
     echo "$logPath not found"
   else
-    gitPing=`ping -c1 $gitHost`
-    gitOk=`echo $gitPing | grep "\s0% packet loss"  | wc -l`
+    gitRevListCount=`git ls-remote --tags $origin | wc -l`
 
     msgNoGit=$dateStamp"\twarning\t$gitHost not reachable."
     msgNoUpdate=$dateStamp"\tlog\tNo update."
-    msgUpdateDone=$dateStamp"\tlog\tUpdate done"
+    msgUpdateReady=$dateStamp"\tlog\tUpdate ready to be installed"
 
-    if [ "$gitOk" -eq 1 ]
+    if [ "$gitRevListCount" -gt 0 ]
     then
 
        if [ "`git config --get user.name`" == "" ]
@@ -58,7 +58,7 @@ then
       # fetch changes, git stores them in FETCH_HEAD
 
       echo "Fetch branch "$currentBranch
-      git fetch origin $currentBranch
+      git fetch $origin $currentBranch
 
       # check for remote changes in origin repository
       newUpdatesAvailable=`git diff HEAD FETCH_HEAD`
@@ -69,7 +69,6 @@ then
         echo "Create fallback"
         # create the fallback
         git checkout -B fallbacks
-
         git add .
         git add -u
         git commit -m $dateStamp
@@ -78,7 +77,7 @@ then
 
         # merged by the user ! git merge FETCH_HEAD
         echo "merged updates"
-        echo -e "$msgUpdateDone" >> $logPath
+        echo -e "$msgUpdateReady" >> $logPath
         
       else
         echo "Git diff HEAD FETCH_HEAD == 0 : no update"
---
+++ update_version.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+#
+# Script to build and push new image in remote docker repository
+#
+BRANCH=$(git branch | grep '*' |awk '{ print $2}')
+REMOTE=origin
+NEW_VERSION=$1
+OLD_VERSION=`cat version.txt`
+echo $NEW_VERSION
+FG_GREEN="\033[32m"
+FG_NORMAL="\033[0m"
+CHANGES_CHECK=$(git status --porcelain | wc -l)
+CUR_HASH=$(git rev-parse HEAD)
+USAGE="Usage : bash build.sh $OLD_VERSION"
+
+if [ $CHANGES_CHECK -gt 0 ]
+then 
+  echo "This project as uncommited changes, stop here"
+  exit 1
+fi
+
+if [ -z "$NEW_VERSION" ] || [ "$NEW_VERSION" == "$OLD_VERSION" ]
+then
+  echo "Wrong or missing version. Old version version =  $OLD_VERSION new version = $NEW_VERSION"
+  echo "$USAGE"
+  exit 1
+fi
+
+#
+# Confirm start
+#
+echo "This script will produce a new version $FG_GREEN$NEW_VERSION$FG_NORMAL and push it on branch $FG_GREEN$BRANCH$FG_NORMAL. Continue ? [YES/NO]"
+read confirm_start
+
+if [ "$confirm_start" != "YES"  ]
+then 
+  echo "Cancel" 
+  exit 1
+fi
+
+#
+# Update version + verification
+#
+echo "Update version.txt"
+REP_VERSION='s/'"$OLD_VERSION"'/'"$NEW_VERSION"'/g'
+perl -pi -e "$REP_VERSION"  version.txt
+
+echo "Write changes"
+vim changes.md
+
+echo "Get diff"
+git --no-pager diff --minimal
+
+echo "Verify git diff of versioning changes. Continue ? [YES/NO]"
+read confirm_diff
+
+if [ "$confirm_diff" != "YES"  ]
+then 
+  echo "Stop here, stash changes. rollback to $CUR_HASH " 
+  git stash
+  exit 1
+fi
+
+exit 1;
+
+git add .
+git commit -m "version $NEW_VERSION"
+git tag $NEW_VERSION
+git push $REMOTE $BRANCH --tags
+
+echo "Done"
+
