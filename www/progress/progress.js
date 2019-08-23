7f33a735c09d2caf9d82ae2c24d993714168dde2
fxi <f@fxi.io>
2019-08-23 18:43:30 +0200

    Refresh style for progress bar code; set title and text as separate elements.

--- www/progress/progress.js
+++ www/progress/progress.js
@@ -1,118 +1,122 @@
-
-
-
-var stopProcess =  function(stop){
+var stopProcess = function(stop) {
   var action = false;
-  if( stop ){
-  action = true;
+  if (stop) {
+    action = true;
   }
-  console.log("stopProcess= "+action);
-  Shiny.onInputChange('cleanExit',action);  
+  console.log('stopProcess= ' + action);
+  Shiny.onInputChange('cleanExit', action);
 };
-
-
-
 
 /**
  * Create and manage multiple progression bar
- * @param {boolean} enable Enable the screen 
+ * @param {boolean} enable Enable the screen
  * @param {string} id Identifier of the given item
  * @param {number} percent Progress bar percentage
+ * @param {string} title Optional title
  * @param {string} text Optional text
  * @param {function} stopFunction Display a button and launch this function if provided
  */
-var progressScreen = function(enable, id, percent, text, stopFunction ) {
-
-  var lScreen = document.getElementsByClassName("loading-screen")[0],
-  lItem = document.getElementById(id),
-  lBusy = document.getElementsByClassName("shiny-busy-panel")[0],
-  lBody = document.getElementsByTagName("body")[0]; 
-
+var progressScreen = function(enable, id, percent, title, text, stopFunction) {
+  var lScreen = document.getElementsByClassName('loading-screen')[0],
+    lItem = document.getElementById(id),
+    lBusy = document.getElementsByClassName('shiny-busy-panel')[0],
+    lBody = document.getElementsByTagName('body')[0];
 
   if (!enable) {
-    if (lScreen) lScreen.remove();
+    if (lScreen) {
+      lScreen.remove();
+    }
     return;
   }
 
-  if (!id || !percent || !text) return;
+  if (!id || !percent || !text) {
+    return;
+  }
 
   if (!lScreen && enable) {
-    lScreen = document.createElement("div");
-    lScreen.className = "loading-screen";
-    lScreenContainer = document.createElement("div");
-    lScreenContainer.className = "loading-container";
+    lScreen = document.createElement('div');
+    lScreen.className = 'loading-screen';
+    lScreenContainer = document.createElement('div');
+    lScreenContainer.className = 'loading-container';
     lScreen.appendChild(lScreenContainer);
-    if(!lBusy){ 
+    if (!lBusy) {
       lBody.appendChild(lScreen);
-    }else{
-      lBody.insertBefore(lScreen,lBusy);
+    } else {
+      lBody.insertBefore(lScreen, lBusy);
     }
   }
 
   if (!lItem) {
     //
-    lItem = document.createElement("div");
-    btnStop = document.createElement("i");
-    pBarIn = document.createElement("div");
-    pBarOut = document.createElement("div");
-    pBarTxt = document.createElement("div");
-    pBarTxtSpan =  document.createElement("span");
+    lItem = document.createElement('div');
+    btnStop = document.createElement('i');
+    pBarIn = document.createElement('div');
+    pBarOut = document.createElement('div');
+    pBarTxt = document.createElement('div');
+    pBarTitleSpan = document.createElement('span');
+    pBarTxtSpan = document.createElement('span');
     //
-    lItem.className = "loading-item";
-    lItem.setAttribute("id", id);
-    pBarIn.className = "loading-bar-in";
-    pBarOut.className = "loading-bar-out";
-    pBarTxt.className = "loading-bar-txt";
-    pBarTxtSpan.className = "loading-bar-txt-content";
+    lItem.className = 'loading-item';
+    lItem.setAttribute('id', id);
+    pBarIn.className = 'loading-bar-in';
+    pBarOut.className = 'loading-bar-out';
+    pBarTxt.className = 'loading-bar-txt';
+    pBarTxtSpan.className = 'loading-bar-txt-content';
+    pBarTitleSpan.className = 'loading-bar-title-content';
+    pBarTxtSpan.style.fontSize = '0.9em';
+    pBarTxtSpan.style.marginLeft = '5px';
     //
     pBarOut.appendChild(pBarIn);
     lItem.appendChild(pBarOut);
     lItem.appendChild(pBarTxt);
     lScreenContainer.appendChild(lItem);
 
-    if (stopFunction){
-      fun = function(){
-        pBarTxtSpan.innerText = "Stop as soon as possible...";
+    if (stopFunction) {
+      fun = function() {
+        pBarTxtSpan.innerText = 'Stop as soon as possible...';
         stopFunction(true);
       };
-      btnStop.setAttribute("class","fa fa-stop-circle");
+      btnStop.setAttribute('class', 'fa fa-stop-circle');
       btnStop.addEventListener('click', fun, false);
     }
 
     pBarTxt.appendChild(btnStop);
-    pBarTxt.appendChild(pBarTxtSpan);  
-
+    pBarTxt.appendChild(pBarTitleSpan);
+    pBarTxt.appendChild(pBarTxtSpan);
   } else {
-    pBarIn = lItem.getElementsByClassName("loading-bar-in")[0];
-    pBarTxtSpan = lItem.getElementsByClassName("loading-bar-txt-content")[0];
+    pBarIn = lItem.getElementsByClassName('loading-bar-in')[0];
+    pBarTxtSpan = lItem.getElementsByClassName('loading-bar-txt-content')[0];
+    pBarTitleSpan = lItem.getElementsByClassName('loading-bar-title-content')[0];
   }
 
   if (percent >= 100) {
-    if (lItem) lItem.remove();
+    if (lItem) {
+      lItem.remove();
+    }
     stopProcess(false);
   } else {
-    pBarIn.style.width = percent + "%";
+    pBarIn.style.width = percent + '%';
+    pBarTitleSpan.innerHTML = title;
     pBarTxtSpan.innerHTML = text;
   }
 
-  lItems = lScreenContainer.getElementsByClassName("loading-item");
+  lItems = lScreenContainer.getElementsByClassName('loading-item');
 
-  if (lItems.length === 0) progressScreen(false);
-
+  if (lItems.length === 0) {
+    progressScreen(false);
+  }
 };
 
-
-$(document).ready(function(){
-
+$(document).ready(function() {
   /* create panel busy*/
-  var body = document.getElementsByTagName("body")[0];
-  var panelBusy = document.createElement("div");
-  var panelBusyContent = document.createElement("div");
-  var panelBusyText =  document.createElement("p");
+  var body = document.getElementsByTagName('body')[0];
+  var panelBusy = document.createElement('div');
+  var panelBusyContent = document.createElement('div');
+  var panelBusyText = document.createElement('p');
 
-  panelBusyText.innerHTML = "Loading, please wait";
-  panelBusy.setAttribute("class","shiny-busy-panel");
-  panelBusyContent.setAttribute("class","shiny-busy-panel-content");
+  panelBusyText.innerHTML = 'Loading, please wait';
+  panelBusy.setAttribute('class', 'shiny-busy-panel');
+  panelBusyContent.setAttribute('class', 'shiny-busy-panel-content');
 
   panelBusyContent.appendChild(panelBusyText);
   panelBusy.appendChild(panelBusyContent);
@@ -120,13 +124,11 @@ $(document).ready(function(){
 
   //var progressOld = {};
 
-  function progressUpdate(m){
+  function progressUpdate(m) {
     m.title = decodeURIComponent(escape(window.atob(m.title)));
     m.text = decodeURIComponent(escape(window.atob(m.text)));
-    text = m.title + ": " + m.text;
-    progressScreen(true,"shinyProgressBar",m.percent,text,stopProcess);
+    progressScreen(true, 'shinyProgressBar', m.percent, m.title, m.text, stopProcess);
   }
 
-  Shiny.addCustomMessageHandler("progressUpdate",progressUpdate);
-
+  Shiny.addCustomMessageHandler('progressUpdate', progressUpdate);
 });
