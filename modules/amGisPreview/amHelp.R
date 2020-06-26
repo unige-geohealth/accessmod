e77dc7da8586f212192c814118612964a85bf073
fxi <f@fxi.io>
2020-06-26 13:55:21 +0200

    Added license header on all .R and .js files written by accessmod team; added WHO terms of use;

--- modules/amGisPreview/amHelp.R
+++ modules/amGisPreview/amHelp.R
@@ -1,16 +1,1 @@
 
-#NOTE: Moved in amUi as text-muted
-#observeEvent(input$helpLinkRelocateMode,{
-  #content <- tagList(
-    
-    #)
-
-  #amUpdateModal(
-    #panelId = 'amHelpPanel',
-    #title = config$helpTitle,
-    #html = content
-    #)
-#})
-
-
-
--- tools/R/amAnalysisReplay.R
+++ tools/R/amAnalysisReplay.R
@@ -1,3 +1,27 @@
+#         ___                                  __  ___            __   ______
+#        /   |  _____ _____ ___   _____ _____ /  |/  /____   ____/ /  / ____/
+#       / /| | / ___// ___// _ \ / ___// ___// /|_/ // __ \ / __  /  /___ \
+#      / ___ |/ /__ / /__ /  __/(__  )(__  )/ /  / // /_/ // /_/ /  ____/ /
+#     /_/  |_|\___/ \___/ \___//____//____//_/  /_/ \____/ \__,_/  /_____/
+#
+#    AccessMod 5 Supporting Universal Health Coverage by modelling physical accessibility to health care
+#    
+#    Copyright (c) 2014-2020  WHO, Frederic Moser (GeoHealth group, University of Geneva)
+#    
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#    
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#    
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# Save and replay analysis
 
 amAnalysisGetPath <- function(name=NULL){
   cacheDir <- config$pathCacheDir
