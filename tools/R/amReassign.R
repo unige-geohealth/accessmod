1febea2451fa1b6b4a168bf09e6ac1716f1c7708
fxi <f@fxi.io>
2022-03-14 11:37:05 +0100

    format

--- tools/R/amReassign.R
+++ tools/R/amReassign.R
@@ -1,28 +1,31 @@
 #'
-#' Hack space : 
+#' Hack space :
 #'  - help debug base function
-#'  - ovewrite package functions 
+#'  - ovewrite package functions
 #'  - additional behaviour
-#'  - etc.. 
+#'  - etc..
 #'
 
 #' Reassign object in given package (hack)
-#' -> replacement can access original version using _<suffix>. Ex. is.finite_orig(...)
-#' 
-#' @param pkgName Package name 
+#' -> replacement can access original version
+#'    using _<suffix>. Ex. is.finite_orig(...)
+#'
+#' @param pkgName Package name
 #' @param name Object name
 #' @param repl Replacement
 #' @return void
-#' @examples 
-#' amReasign('base','is.finite',function(...){cat('hey',is.finite_orig(...))})
+#' @examples
+#' amReasign("base", "is.finite", function(...) {
+#'   cat("hey", is.finite_orig(...))
+#' })
 #'
-amReasign <- function(pkgName,name,repl,suffix="orig"){
-  ePkg <- as.environment(sprintf('package:%s',pkgName))
+amReasign <- function(pkgName, name, repl, suffix = "orig") {
+  ePkg <- as.environment(sprintf("package:%s", pkgName))
   eGlob <- globalenv()
-  name_orig <-sprintf("%s_%s",name,suffix)
+  name_orig <- sprintf("%s_%s", name, suffix)
   has_bkp <- !is.null(eGlob[[name_orig]])
 
-  if(!has_bkp){
+  if (!has_bkp) {
     assign(
       x = name_orig,
       value = ePkg[[name]],
@@ -30,7 +33,7 @@ amReasign <- function(pkgName,name,repl,suffix="orig"){
     )
   }
 
-  unlockBinding( name, ePkg )
+  unlockBinding(name, ePkg)
 
   assign(
     x = name,
@@ -38,24 +41,24 @@ amReasign <- function(pkgName,name,repl,suffix="orig"){
     envir = ePkg
   )
 
-  lockBinding(name,ePkg)
+  lockBinding(name, ePkg)
 }
 
-#' Reset TZ env variable before format 
+#' Reset TZ env variable before format
 #' ⚠️ Something write TZ env variable and corrupts it !
 #' probably https://github.com/wch/r-source/blob/tags/R-4-1-1/src/main/datetime.c
 #' We can't do anything excepts rewrite back the correct TZ...
-reset_tz <- function(){
-  tz <- options('tz')
-  if(is.null(tz)){
-    tz = 'UTC'
+reset_tz <- function() {
+  tz <- options("tz")
+  if (is.null(tz)) {
+    tz <- "UTC"
   }
-  if(!identical(Sys.getenv('TZ'),tz)){
-    Sys.setenv(TZ=tz);  
+  if (!identical(Sys.getenv("TZ"), tz)) {
+    Sys.setenv(TZ = tz)
   }
 }
 
-amReasign('base','format',function(...){
+amReasign("base", "format", function(...) {
   reset_tz()
   format_orig(...)
 })
@@ -65,20 +68,21 @@ amReasign('base','format',function(...){
 #' gsub will fail if encoding is not utf8: it happens
 #' with the TZ issue. Uncomment this to intercept here
 #'
-if(FALSE){
+if (FALSE) {
   # if TRUE : debug mode
-  amReasign('base','gsub',function(...){
-    tryCatch({
-      gsub_orig(...)
-    },
-    error = function(e){
-      #
-      # Use parent.frame(1-4) and .tracestac(1-5) to
-      # to inspect what's wrong
-      #
-      browser()
-      stop(e)
-    })
-})
+  amReasign("base", "gsub", function(...) {
+    tryCatch(
+      {
+        gsub_orig(...)
+      },
+      error = function(e) {
+        #
+        # Use parent.frame(1-4) and .tracestac(1-5) to
+        # to inspect what's wrong
+        #
+        browser()
+        stop(e)
+      }
+    )
+  })
 }
-
