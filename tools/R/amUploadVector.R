5467edfaa5f68bc504159a682555dcfb2acf6d7c
fxi <f@fxi.io>
2024-10-30 20:04:49 +0100

    vect_proj / vect_upload mixed up

--- tools/R/amUploadVector.R
+++ tools/R/amUploadVector.R
@@ -10,48 +10,37 @@ amUploadVector <- function(dataInput, dataName, dataFiles, pBarTitle) {
       }
     }
   })
-
-
   progressBarControl(
     visible = TRUE,
     percent = 20,
     title = pBarTitle,
     text = "Attributes validation and cleaning"
   )
-
   # validate if multifiles
   amValidateFileExt(dataFiles, "vect")
-
   #
   # Comparison / extent validation
   #
   loc_meta <- amMapMeta()
   loc_proj <- loc_meta$orig$proj
   loc_bbox <- loc_meta$bbxSp$orig
-
   vect_upload <- vect(dataInput)
   vect_proj <- crs(vect_upload)
   vect_bbox <- as.polygons(ext(vect_upload), crs = crs(vect_upload))
-
   proj_match <- st_crs(vect_bbox) == st_crs(loc_proj)
-
   if (!proj_match) {
-    vect_proj <- project(vect_upload, loc_proj)
+    vect_upload <- project(vect_upload, loc_proj)
   }
-
-  extent_match <- amExtentsMatch(loc_bbox, vect_proj)
-
+  extent_match <- amExtentsMatch(loc_bbox, vect_upload)
   if (!extent_match) {
     stop("Imported vector extent is not within location extent")
   }
-
-  vect_proj <- vect_proj[, !names(vect_proj) %in% c("cat", "cat_")]
-
+  # Remove cat and cat_ columns if they exist
+  vect_upload <- vect_upload[, !names(vect_upload) %in% c("cat", "cat_")]
   write_VECT(
-    vect_proj,
+    vect_upload,
     dataName,
     flags = c("overwrite")
   )
-
   return(NULL)
 }
