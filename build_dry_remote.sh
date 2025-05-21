3ed3ec6bf1d633f482da1db9ff1ee51ce0eef468
fxi <f@fxi.io>
2025-05-21 16:36:47 +0200

    ci(workflow): unify Docker and QEMU builds in single pipeline with consistent versioning

--- build_dry_remote.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cur=$(git branch --show-current)
-gh workflow run build_accessmod --ref $cur  --field dry_run=true
--- qemu/build_remote.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cur=$(git branch --show-current)
-gh workflow run build_qemu --ref $cur -f version=${1:-5.9}
